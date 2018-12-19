9900749d9d65e51782dac479591b96a2b6508976
luka-saravanja <luka.saravanja67@gmail.com>
2018-12-19 10:48:15 +0100

    Add files via upload
    Function for printing linked list members

--- Source.c
+++ Source.c
@@ -10,22 +10,33 @@ struct Node
 
 struct Node* head; // pointer to head of doubly linked list
 
-struct Node* createNewNode(int x) {
-	struct Node* newNode
-		= (struct Node*)malloc(sizeof(struct Node));
+struct Node* createNewNode(int x)
+{
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
 	newNode->data = x;
 	newNode->prev = NULL;
 	newNode->next = NULL;
 	return newNode;
 } // kreira novi Node i vrati pointer na njega,odma alocira onoliko memorije koliko zauzima Node struktura,pošto je prvi node previous node i next node pointer su null
 
-void insAtHead(int x) {
+void insAtHead(int x) 
+{
 	struct Node* newNode = createNewNode(x);
-	if (head == NULL) {
+	if (head == NULL)
+	{
 		head = newNode;
 		return;
 	}
 	head->prev = newNode;
 	newNode->next = head;
 	head = newNode;
-} // dodaje novi Node na head liste
+}// dodaje novi Node na head liste
+
+void printList() {
+	struct Node* temp = head;
+	while (temp != NULL) {
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
+}
